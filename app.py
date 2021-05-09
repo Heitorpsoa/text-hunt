82324ce293cc382059cc2497aee75ff898160d68
heitorpsoa <heitorpsoa@gmail.com>
2021-05-09 20:54:16 -0300

    first release

--- app.py
+++ app.py
@@ -3,39 +3,43 @@ from controller import cont
 
 app = flask.Flask(__name__)
 
+_cont = cont.Controller()
+
 @app.route('/', methods=["GET"])
 def home():
     return flask.render_template('home.html')
 
 @app.route('/originals', methods=["GET"])
 def originals():
-    texts = cont.getRawTexts()
+    texts = _cont.getRawTexts()
     return flask.render_template('texts.html', texts = texts)
 
 @app.route('/processed', methods=["GET"])
 def processeds():
-    texts = cont.getProcessedTexts()
+    texts = _cont.getProcessedTexts()
     return flask.render_template("texts.html", texts = texts)
 
 @app.route('/indexation', methods=["GET"])
 def indexation():
-    index = cont.indexation()
+    index = _cont.index
     return flask.render_template("indexation.html", index = index)
 
 @app.route('/search', methods=["GET"])
 def search():
-    tags = cont.getAllTags()
+    tags = _cont.getAllTags()
     return flask.render_template("search.html", tags = tags)
 
-@app.route('/search/go', methods=["get"])
+@app.route('/search/go', methods=["POST"])
 def searchGo():
-    term = flask.request.args.get('term')
-    result = cont.search(term)
+    term = flask.request.form['term']
+    tags = flask.request.form.getlist('tags')
+
+    result = _cont.search(term, tags)
     return flask.render_template('result.html', result = result, term = term)
 
 @app.route('/search/redirect/<int:documentIndex>/<string:term>', methods=["GET"])
 def redirect(documentIndex, term):
-    text = cont.getText(documentIndex)
+    text = _cont.getText(documentIndex)
     return flask.render_template('single-text.html', text=text, term=term) 
 
 
--- controller/cont.py
+++ controller/cont.py
@@ -2,170 +2,242 @@ import os
 import time
 from controller import stopwords
 
-
-def getRawTexts():
-    users = os.listdir('users')
-    texts = []
-    
-    # Cada usuário
-    for u in users:
-
-        # Lista de arquivos de cada usuário (text1, text2, tags1, tags2... textn, tagsn)
-        files = os.listdir('users/'+u)
-        for i in range(int((len(files) + 1) / 2)):
-
-            # Exemplo: users/user2/text3 
-            textPath = os.path.join('users', u, "text"+str(i+1))
-            tagsPath = os.path.join('users', u, "tags"+str(i+1))
-
-            text = open(textPath, "r")
-            tags = open(tagsPath, "r")
-
-            text = text.read()
-            tags = tags.read()
-
-
-            texts.append({
-                "user": u,
-                "data": text,
-                "tags": tags
-            })
-
-    
-    return texts
-
-def processWord(word):
-    processedWord = word.lower()
-
-    #Tirando pontuação grudada na palavra
-    processedWord = processedWord.replace(',', '')
-    processedWord = processedWord.replace('.', '')
-    processedWord = processedWord.replace('“', '')
-    processedWord = processedWord.replace('"', '')
-
-    #Substituição Semântica
-    if processedWord.isnumeric():
-        processedWord = "number " + processedWord
-    if "%" in processedWord:
-        processedWord = "percent " + processedWord
-    if "$" in processedWord:
-        processedWord = "money " + processedWord 
-
-    # Retorna backspace para apagar o espaço adicionado
-    if processedWord in stopwords.words:
-        return False
-    return processedWord
-
-def processText(text): 
-    words = text.split()
-    processedWords = ""
-    for word in words:
-        w = processWord(word)
-        if w:
-            processedWords += w + ' '
-    return processedWords
-
-
-
-# Retorna os textos processados, objetivo é salvar posteriormente
-def getProcessedTexts():
-    texts = getRawTexts()
-    processed = []
-    for t in texts:
-        processed.append({
-            'processed': processText(t['data']),
-            'user': t['user'],
-            'data': t['data'],
-            'tags': t['tags']
-        }) 
-    return processed
-
-
-def indexation(): 
-    texts = getProcessedTexts()
+class Controller:
     index = {}
+    documents = []
 
-    # Índice do documento
-    i = 0
-    for t in texts:
-        words = t['processed'].split(' ')
+    def __init__(self):
+        self.index = self.indexation()
+        self.raw = self.getRawTexts()
+        self.documents = self.getProcessedTexts()
+
+    def getRawTexts(self):
+        users = os.listdir('users')
+        texts = []
         
-        # Índice da palavra
-        j = 0
-        for w in words:
-            
-            # Palavras Vazias exisitiam e não bugavam o sistema :O
-            if w == '':
-                continue
-            if w not in index:
-                index[w] = {'occurences': [], 'tagsWeight': {}}
-                index[w]['occurences'].append((i,j))
-            else:
-                index[w]['occurences'].append((i,j))
+        # Cada usuário
+        for u in users:
 
-            # Adicionando peso para as tags do texto atual
-            tags = t['tags'].split('\n')
-            for tag in tags:
-                if tag in index[w]['tagsWeight']:
-                    index[w]['tagsWeight'][tag] = index[w]['tagsWeight'][tag] + 1
+            # Lista de arquivos de cada usuário (text1, text2, tags1, tags2... textn, tagsn)
+            files = os.listdir('users/'+u)
+
+            for i in range(int((len(files) + 1) / 2)):
+
+                # Exemplo: users/user2/text3 
+                textPath = os.path.join('users', u, "text"+str(i+1) + '.txt')
+                tagsPath = os.path.join('users', u, "tags"+str(i+1) + '.txt')
+            
+                text = open(textPath, "r")
+                tags = open(tagsPath, "r")
+
+                text = text.read()
+                tags = tags.read()
+
+
+                texts.append({
+                    "user": u,
+                    "data": text,
+                    "tags": tags
+                })
+
+        
+        return texts
+
+    def processWord(self, word):
+        processedWord = word.lower()
+
+        #Tirando pontuação grudada na palavra
+        processedWord = processedWord.replace(',', '')
+        processedWord = processedWord.replace('.', '')
+        processedWord = processedWord.replace('“', '')
+        processedWord = processedWord.replace('"', '')
+
+        #Substituição Semântica
+        if processedWord.isnumeric():
+            processedWord = "number " + processedWord
+        if "%" in processedWord:
+            processedWord = "percent " + processedWord
+        if "$" in processedWord:
+            processedWord = "money " + processedWord 
+
+        # Retorna backspace para apagar o espaço adicionado
+        if processedWord in stopwords.words:
+            return False
+        return processedWord
+
+    def processText(self, text): 
+
+        # Todas as palavras do texto
+        words = text.split()
+        processedWords = ""
+
+        # Para cada palavra
+        for word in words:
+
+            # Limpa a palavra
+            w = self.processWord(word)
+            if w:
+                processedWords += w + ' '
+        return processedWords
+
+
+
+    # Retorna os textos processados, objetivo é salvar posteriormente
+    def getProcessedTexts(self):
+        texts = self.getRawTexts()
+        processed = []
+
+        for t in texts:
+            processed.append({
+                'processed': self.processText(t['data']),
+                'user': t['user'],
+                'data': t['data'],
+                'tags': t['tags']
+            }) 
+        return processed
+
+
+    def indexation(self): 
+        texts = self.getProcessedTexts()
+        index = {}
+
+        # Índice do documento
+        i = 0
+        for t in texts:
+            words = t['processed'].split(' ')
+            
+            # Índice da palavra
+            j = 0
+            for w in words:
+                
+                # Palavras Vazias exisitiam e não bugavam o sistema :O
+                if w == '':
+                    continue
+                    
+                # Se a palavra ainda não está indexada
+                if w not in index:
+                    index[w] = {'occurences': [], 'tagsWeight': {}}
+                    index[w]['occurences'].append((i,j))
                 else:
-                    index[w]['tagsWeight'][tag] = 1
+                    index[w]['occurences'].append((i,j))
 
-            j = j + len(w) + 1
-        i = i + 1
-
-    return index
+                # Adicionando peso para as tags do texto atual
+                tags = t['tags'].split('\n')
 
 
-def getOccurencePreview(index, documents, occurence):
-    documentIndex, startIndex = occurence
-    document =  documents[documentIndex]
-    
-    preview =  "..." + document['processed'][startIndex-20:startIndex+20] + "..."
-    return {"user": document['user'], "preview": preview, "index": documentIndex, "startIndex": startIndex} 
+                for tag in tags:
 
-def search(term):
-    documents = getProcessedTexts()
-    index = indexation()
+                    # Se a tag já existe nos pesos
+                    if tag in index[w]['tagsWeight']:
+                        index[w]['tagsWeight'][tag] = index[w]['tagsWeight'][tag] + 1
+                    else:
+                        index[w]['tagsWeight'][tag] = 1
+                
+                j = j + len(w) + 1
+            i = i + 1
 
-    # Indexação não conta para o tempo de pesquisa
-    start = time.time()
-    cont = 0
-    occurences = []
+        return index
 
-    for key in index:
 
-        # Se achou o termo 
-        if key == term:
+    # Retorna um objeto de preview para aquela ocorrência
+    def getOccurencePreview(self, documents, occurence):
+        documentIndex, startIndex = occurence
+        document =  documents[documentIndex]
 
-            # Para cada ocorrencia do termo em todos os documentos
-            for occurence in index[key]['occurences']:
-                cont = cont + 1
-                occurences.append(
-                    getOccurencePreview(index, documents, occurence)
-                )
+        preview =  "..." + document['processed'][startIndex-20:startIndex+20] + "..."
+        
+        return {  
+            "user": document['user'],
+            "preview": preview,
+            "index": documentIndex,
+            "startIndex": startIndex,
+        }
 
-    end = time.time()
-    duration = end - start
+    def getOccurenceRelevance(self, index, term, tags, documents, occurence):
+        try:
+            documentIndex, startIndex = occurence
+            document =  documents[documentIndex]
 
-    result = {
-        "cont": cont,
-        "duration": duration,
-        "occurences": occurences
-    }
-    return result
+            tagsWeights = index[term]['tagsWeight']
 
-def getText(docIndex):
-    texts = getRawTexts()
-    return texts[docIndex]
+            score = 0
+            maximum = 0
 
-def getAllTags():
-    texts = getRawTexts()
-    tags = []
-    for tagArray in texts:
-        for t in tagArray['tags'].split('\n'):
-            tags.append(t)
+            # Para cada um das tags weights: [money: 54, finnance: 22 ... ] 
+            for tw in tagsWeights:
+                # O potencial máximo  de relevância aumenta 
+                maximum += tagsWeights[tw]
+                
+                # Para cada tag que o usuário tem interesse [money, terror]
+                for tag in tags:
+
+                    # Se a tag está contida nos pesos indexados
+                    if tag in tw:
+                        score += tagsWeights[tw]
             
-    # Removendo duplicatas        
-    tags = list(dict.fromkeys(tags))    
-    return tags
+            # Relevância relativa
+            percent = (score / maximum) * 100
+            return {
+                "score": score,
+                "wordScore": maximum,
+                "percent": "{:.2f}".format(percent)
+            }
+
+        except Exception as e:
+            print(e)
+            return e
+
+    def search(self, term, tags):
+        documents = self.documents
+        index = self.index
+
+        # Indexação não conta para o tempo de pesquisa
+        start = time.time()
+        cont = 0
+        occurences = []
+
+        for key in index:
+
+            # Se achou o termo 
+            if key == term:
+
+                # Para cada ocorrencia do termo em todos os documentos
+                for occurence in index[key]['occurences']:
+                    cont = cont + 1
+                    
+                    ocuResult = self.getOccurencePreview(documents, occurence)
+                    ocuResult['relevance'] = self.getOccurenceRelevance(index, term, tags, documents, occurence)
+                    occurences.append(
+                        ocuResult
+                    )
+        
+        # Ordenando por relevância
+        def sortFunc(el):
+            return el['relevance']['score']
+        occurences.sort(key=sortFunc, reverse=True)
+
+        end = time.time()
+        duration = end - start
+
+        result = {
+            "cont": cont,
+            "duration": duration,
+            "occurences": occurences,
+        }
+        return result
+
+    def getText(self, docIndex):
+        texts = self.getRawTexts()
+        return texts[docIndex]
+
+    def getAllTags(self):
+        texts = self.getRawTexts()
+        tags = []
+        for tagArray in texts:
+            for t in tagArray['tags'].split('\n'):
+                tags.append(t)
+                
+        # Removendo duplicatas        
+        tags = list(dict.fromkeys(tags))    
+        return tags
+
